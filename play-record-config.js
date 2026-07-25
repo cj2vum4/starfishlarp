83b04dcec27d322feeb0e7ccc4ae0606a888c7f3
cj2vum4 <cj2vum4@gmail.com>
2026-07-26 01:41:33 +0800

    新增玩本記錄端點設定檔

---
+++ play-record-config.js
@@ -0,0 +1,9 @@
+/*
+ * 將 Google Apps Script 部署成「網頁應用程式」後，
+ * 把 /exec 結尾的網址貼在下方。
+ *
+ * 範例：
+ * window.STARFISH_PLAY_RECORD_ENDPOINT =
+ *   'https://script.google.com/macros/s/AKfycb.../exec';
+ */
+window.STARFISH_PLAY_RECORD_ENDPOINT = '';
